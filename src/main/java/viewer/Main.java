76ca12a229298f51c5eba64800c7f92643b10eca
Janne Korhonen <janne.olavi3@gmail.com>
2019-08-05 17:47:03 +0300

    initial commit

---
+++ src/main/java/viewer/Main.java
@@ -0,0 +1,23 @@
+package viewer;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Label label = new Label("hello");
+        Scene scene = new Scene(label);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+    }
+}
