e7c240b454e1437de22acec8609dc8dfa0114fa1
Janne Korhonen <janne.olavi3@gmail.com>
2019-08-14 18:50:50 +0300

    javalite http

---
+++ src/main/java/viewer/Controller.java
@@ -0,0 +1,32 @@
+package viewer;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import org.javalite.http.Get;
+import org.javalite.http.Http;
+import javafx.scene.control.Label;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML private Label labeltest;
+    @FXML private Button buttontest;
+
+    public void buttonClicked() {
+
+
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
+
+
+}
--- src/main/java/viewer/Main.java
+++ src/main/java/viewer/Main.java
@@ -1,23 +1,29 @@
 package viewer;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("/MainLayout.fxml"));
+        primaryStage.setTitle("Nasa viewer");
+        primaryStage.setScene(new Scene(root, 1000 , 600));
+        primaryStage.show();
+
+    }
+
+
+
     public static void main(String[] args) {
         launch(args);
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
-        Label label = new Label("hello");
-        Scene scene = new Scene(label);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-
-    }
 }
