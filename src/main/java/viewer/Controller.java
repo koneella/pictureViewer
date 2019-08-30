e7ae96c0347f1b288b0c9b0d09b6474b40b9c1a3
Janne Korhonen <janne.olavi3@gmail.com>
2019-08-30 13:26:41 +0300

    json parsing

--- src/main/java/viewer/Controller.java
+++ src/main/java/viewer/Controller.java
@@ -3,11 +3,13 @@ package viewer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import org.javalite.http.Get;
 import org.javalite.http.Http;
-import javafx.scene.control.Label;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,6 +20,29 @@ public class Controller implements Initializable {
 
     public void buttonClicked() {
 
+        try {
+            Get get = Http.get("");
+
+           JSONObject jsonObject = new JSONObject(get.text());
+           //String test = jsonObject.getJSONObject("collection").getString("media_type");
+
+           //System.out.println(test);
+            JSONArray arr = jsonObject.getJSONArray("");
+            for (int i = 0; i < arr.length(); i++) {
+                String center = arr.getJSONObject(i).getString("center");
+                System.out.println(center);
+
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+
+      //  System.out.println(get.text());
+       // System.out.println(get.responseCode());
 
     }
 
